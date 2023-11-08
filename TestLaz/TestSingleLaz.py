135f258a5d07b5b1f2e0b796c1bcbea2139877b7
Mertcan Erdem <92017528+MertcanErdem@users.noreply.github.com>
2023-11-08 13:33:36 +0300

    Add files via upload

--- TestLaz/TestSingleLaz.py
+++ TestLaz/TestSingleLaz.py
@@ -1,5 +1,40 @@
-import numpy as np
 import geopandas as gpd
+import LasBuildSeg as Lasb
+import numpy as np
+
+# Define input parameters
+input_laz = 'USGS_LPC_IL_HicksDome_FluorsparDistrict_2019_D19_2339_5650.laz'  # Path to the input laz/las data file
+GroundTruth = "USGS_LPC_IL_HicksDome_FluorsparDistrict_2019_D19_2339_5650_gt_buildings.geojson"  # Path to Your Ground Truth
+epsg_code = 6457  # EPSG code of the input laz data
+intermethod = 'nearest'  # Interpolation method ('cubic', 'nearest', or 'linear')
+
+# You can change this paramets to see how it effects the building maps
+constant = 3.6  # Adaptive threshold constant
+block_size = 91  # Adaptive threshold block size
+kernel_size = 3  # Morphological open kernel size
+tri_threshold = 4  # Terrain Ruggedness Index threshold
+multy = 1200  # Multiplication factor for DSM height enhancement
+output_number = 11 # use this variable so you can change name of every output you get automaticly
+
+
+# Generate DSM and DTM
+Lasb.generate_dsm(input_laz, epsg_code, intermethod)
+Lasb.generate_dtm(input_laz, epsg_code, intermethod, multy)
+
+# Generate NDHM
+Lasb.generate_ndhm('dtm.tif', 'dsm.tif')
+
+import sys, os
+
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+
 
 def calc_iou(gdf_groundtruth, gdf_predict):
     """Calculates intersection over union (iou) score.
@@ -51,83 +86,68 @@ def calc_metrics(groundtruth_file, predict_file):
         iou = iou * (len(gdf_groundtruth)/len(gdf_predict))
     
     return iou
-   
+    
 
-
-import LasBuildSeg as Lasb
-import numpy as np
-
-# Define input parameters
-input_laz = "USGS_LPC_IL_HicksDome_FluorsparDistrict_2019_D19_2339_5650.laz" # Path to the input to Your Point Cloud data in .laz/.las format.
-GroundTruth = "USGS_LPC_IL_HicksDome_FluorsparDistrict_2019_D19_2339_5650_gt_buildings.geojson"  # Path to the input to Your Ground Truth file
-epsg_code = 6457 # EPSG code of the input laz data
-multy = 1200  # Multiplication factor for DSM height enhancement
-intermethod = 'nearest'  # Interpolation method ('cubic', 'nearest', or 'linear')
-
-
-# Define output parameters
-output_number = 1  # Use this variable to automatically name the output files (change it for each new input laz file)
-constant = 3.6  # Adaptive threshold constant
-block_size = 91  # Adaptive threshold block size
-kernel_size = 3  # Morphological open kernel size
-tri_threshold = 3 # Terrain Ruggedness Index threshold
-
+# Define contour filtering parameters (You dont need to change this ones)
 min_size = 35
 max_size = 5000
 squareness_threshold = 0.3
 width_threshold = 3
 height_threshold = 3
+CloseKernel_size = 15
 
-# Generate DSM and DTM
-Lasb.generate_dsm(input_laz, epsg_code, intermethod)
-Lasb.generate_dtm(input_laz, epsg_code, intermethod, multy)
 
-# Generate NDHM
-Lasb.generate_ndhm('dtm.tif', 'dsm.tif')
 
-# Read NDHM image and profile
+
+
+
 img, profile = Lasb.read_geotiff('ndhm.tif')
-
-# Transform DSM
 Lasb.DSM_transform('dsm.tif')
-
-# Read transformed DSM and profile
 dem, _ = Lasb.read_geotiff('dsm3857.tif')
-
-# Convert image to 8-bit
 img_8bit = Lasb.to_8bit(img)
 
-# Apply adaptive thresholding
 img_thresh = Lasb.threshold(img_8bit, block_size, constant)
-Lasb.write_geotiff('img_thresh_' + str(output_number) + '.tif', img_thresh, profile)
-
-# Apply morphological opening
 img_open = Lasb.morph_open(img_thresh, kernel_size)
-Lasb.write_geotiff('img_open_' + str(output_number) + '.tif', img_open, profile)
 
-# Generate building footprints without TRI
-building_masknotri = Lasb.filter_contoursntri(img_open, profile, min_size, max_size, squareness_threshold, width_threshold, height_threshold)
-Lasb.write_geotiff('building_maskNOtri_' + str(output_number) + '.tif', building_masknotri, profile)
-Lasb.building_footprints_to_geojson('building_maskNOtri_' + str(output_number) + '.tif', 'building_maskNOtri_' + str(output_number) + '.geojson')
+blockPrint()
+import os
 
-# Generate building footprints with TRI
+# Create output folders for each step if they don't exist
+output_base_dir = 'output'  # Change this to your desired output base directory
+os.makedirs(output_base_dir, exist_ok=True)
+
+# Function to write files to the appropriate folder
+def write_output(filename, data, profile, folder_name):
+    output_folder = os.path.join(output_base_dir, folder_name)
+    os.makedirs(output_folder, exist_ok=True)
+    file_path = os.path.join(output_folder, filename)
+    Lasb.write_geotiff(file_path, data, profile)
+    Lasb.building_footprints_to_geojson(file_path, file_path.replace('.tif', '.geojson'))
+
+
+# S1 Step
+building_mask = Lasb.filter_contoursntri(img_open, profile, min_size, max_size, squareness_threshold, width_threshold, height_threshold)
+write_output('S1_Contour_' + str(output_number) + '.tif', building_mask, profile, 'S1_Contour')
+
+# S2 Step
+dem, _ = Lasb.read_geotiff('dsm3857.tif')
 building_mask = Lasb.filter_contours(img_open, dem, profile, min_size, max_size, squareness_threshold, width_threshold, height_threshold, tri_threshold)
-Lasb.write_geotiff('building_masktri_' + str(output_number) + '.tif', building_mask, profile)
-Lasb.building_footprints_to_geojson('building_masktri_' + str(output_number) + '.tif', 'building_masktri_' + str(output_number) + '.geojson')
+write_output('S2_TRI_' + str(output_number) + '.tif', building_mask, profile, 'S2_TRI')
 
-# Apply morphological closing
-CloseKernel_size = 15
-building_mask = Lasb.read_geotiff('building_masktri_' + str(output_number) + '.tif')[0]
+# S3 Step
 building_mask_closed = Lasb.close(building_mask, CloseKernel_size)
-Lasb.write_geotiff('building_mask_Final_' + str(output_number) + '.tif', building_mask_closed, profile)
-Lasb.building_footprints_to_geojson('building_mask_Final_' + str(output_number) + '.tif', 'building_mask_Final_' + str(output_number) + '.geojson')
+write_output('S3_MorphClose_' + str(output_number) + '.tif', building_mask_closed, profile, 'S3_MorphClose')
 
-# Calculate IOU metrics
-notri_IOU = calc_metrics(GroundTruth, 'building_maskNOtri_' + str(output_number) + '.geojson')
-tri_IOU = calc_metrics(GroundTruth, 'building_masktri_' + str(output_number) + '.geojson')
-final_IOU = calc_metrics(GroundTruth, 'building_mask_Final_' + str(output_number) + '.geojson')
+# Make sure to close any open files or resources as needed
 
-# Print IOU results
-print("Contour Detection with no TRI applied IoU:", notri_IOU)
-print("Using Terrain Ruggedness Index IoU:", tri_IOU)
-print("Final results after Morphological Close IoU:", final_IOU)
+
+enablePrint()
+
+
+notri_IOU = calc_metrics(GroundTruth, os.path.join(output_base_dir, 'S1_Contour', 'S1_Contour_' + str(output_number) + '.geojson'))
+tri_IOU = calc_metrics(GroundTruth, os.path.join(output_base_dir, 'S2_TRI', 'S2_TRI_' + str(output_number) + '.geojson'))
+final_IOU= calc_metrics(GroundTruth, os.path.join(output_base_dir, 'S3_MorphClose', 'S3_MorphClose_' + str(output_number) + '.geojson'))
+
+print("S1 Contour Detection Iou %",notri_IOU)
+print("S2 Contour Detection wit TRI Iou %",tri_IOU)
+print("S3 Morphological Close Iou %",final_IOU)
